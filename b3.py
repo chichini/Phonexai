2c282cfdc8cdf5a9e59a90b155554372b2efecf7
chichini <80317616+chichini@users.noreply.github.com>
2021-03-22 15:51:15 +0700

    Add files via upload

---
+++ b3.py
@@ -0,0 +1,5 @@
+n= int(input('Nhap so tu ban phim'))
+if n % 2 == 0:
+    print('So chan');
+else:
+    print ('So le');
---
+++ b5.py
@@ -0,0 +1,4 @@
+n = int(input('Nhap so tu ban phim'));
+while n >= 0:
+    print (n);
+    n = n - 1;
